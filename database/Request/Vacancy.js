5856599e4c7eddbc04f06d2b072d3dfbdfc9da08
Igor <igorushakov111@gmail.com>
2024-09-25 09:51:13 +0700

    Удаление вакансий автоматически

--- database/Request/Vacancy.js
+++ database/Request/Vacancy.js
@@ -2,71 +2,14 @@ const Vacancy = require("../Schema/Vakancy");
 const { v4 } = require("uuid");
 const { Temporal } = require("@js-temporal/polyfill");
 
-async function createVacancy({
-  userID,
-  special,
-  skills,
-  typeWork,
-  experience,
-  price,
-  description,
-  responses,
-}) {
-  const formattedSkills = skills.map((skill) => ({ title: skill }));
-  const formattedTypeWork = typeWork.map((workType) => ({ title: workType }));
-
-  // Использование PlainDateTime для работы с датой и временем
-  const now = Temporal.Now.plainDateTimeISO();
-  const addDate = now.add({ days: 30 });
-
-  const newVacancy = new Vacancy({
-    id: v4(),
-    userID,
-    special,
-    skills: formattedSkills,
-    typeWork: formattedTypeWork,
-    experience,
-    price,
-    description,
-    responses,
-    dateAndTimeCreated: addDate.toString(), // Сохраняем дату как строку
-  });
-
-  try {
-    const savedVacancy = await newVacancy.save();
-    return { success: true, data: savedVacancy };
-  } catch (err) {
-    console.log(err);
-    return { success: false, error: err.message };
-  }
+function getCurrentDateInMSK() {
+  return Temporal.Now.plainDateISO();
 }
 
-async function searchVacancyById(id) {
-  try {
-    const vacancy = await Vacancy.findOne({ id });
-    if (!vacancy) {
-      return { success: false, message: "Вакансии с таким id не найдено" };
-    }
-    return { success: true, data: vacancy };
-  } catch (err) {
-    console.log(err);
-    return { success: false, error: err.message };
-  }
-}
-async function searchVacancyByUserId(id) {
-  try {
-    const vacancy = await Vacancy.find({ userID: id });
-    if (!vacancy) {
-      return {
-        success: false,
-        message: "Вакансий у пользователя с таким id не найдено",
-      };
-    }
-    return { success: true, data: vacancy };
-  } catch (err) {
-    console.log(err);
-    return { success: false, error: err.message };
-  }
-}
+let findVacancyForDelete = async () => {
+  console.log(getCurrentDateInMSK().toString());
+  let currentDate = await getCurrentDateInMSK();
+  return Vacancy.deleteMany({ dateAndTimeCreated: currentDate });
+};
 
-module.exports = { createVacancy, searchVacancyById, searchVacancyByUserId };
+module.exports = { findVacancyForDelete };
--- server.js
+++ server.js
@@ -7,40 +7,40 @@ const {
   payComapany,
   removeOldCompanyRecords,
 } = require("./database/Request/Company");
+const {findVacancyForDelete} = require('./database/Request/Vacancy')
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
-
-schedule.scheduleJob("*/5 * * * * *", async () => {
-  try {
-    await console.log("Проверка подписок на премиум");
-    let findAllPremium = await removePremium();
-    console.log("Обновленные подписки:", findAllPremium);
-  } catch (e) {
-    console.error("Ошибка при проверке подписок:", e);
-  }
-});
-schedule.scheduleJob("*/8 * * * * *", async () => {
-  try {
-    await console.log("Проверка подписки компаний");
-    let removed = await payComapany();
-    console.log("Обновленные компании:", removed);
-  } catch (e) {
-    console.error("Ошибка при проверке компании:", e);
-  }
-});
-
-schedule.scheduleJob("*/12 * * * * *", async () => {
-  try {
-    await console.log("Удаление компаний");
-    let removed = await removeOldCompanyRecords();
-    console.log("Удалили:", removed);
-  } catch (e) {
-    console.error("Ошибка при удалении:", e);
-  }
-});
+findVacancyForDelete()
+// schedule.scheduleJob("*/5 * * * * *", async () => {
+//   try {
+//     await console.log("Проверка подписок на премиум");
+//     let findAllPremium = await removePremium();
+//     console.log("Обновленные подписки:", findAllPremium);
+//   } catch (e) {
+//     console.error("Ошибка при проверке подписок:", e);
+//   }
+// });
+// schedule.scheduleJob("*/8 * * * * *", async () => {
+//   try {
+//     await console.log("Проверка подписки компаний");
+//     let removed = await payComapany();
+//     console.log("Обновленные компании:", removed);
+//   } catch (e) {
+//     console.error("Ошибка при проверке компании:", e);
+//   }
+// });
+// schedule.scheduleJob("*/12 * * * * *", async () => {
+//   try {
+//     await console.log("Удаление компаний");
+//     let removed = await removeOldCompanyRecords();
+//     console.log("Удалили:", removed);
+//   } catch (e) {
+//     console.error("Ошибка при удалении:", e);
+//   }
+// });
 app.listen(process.env.PORT, () => {
   let connect = connectDB();
   console.log(`Server is running on port ${process.env.PORT}`);
